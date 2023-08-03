22ef907a411492496591be84d88a81c68519f9f0
sergpvv <serg.pvv@gmail.com>
2023-08-03 04:18:05 +0000

    wip: edit user

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,27 +12,47 @@ export default (app) => {
     .get('/users/new', { name: 'newUser' }, (req, reply) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
+      return reply;
+    })
+    .get('/users/:id/edit', { name: 'editUser' }, async (req, reply) => {
+      const { id } = req.params;
+      const { body, query, params } = req;
+      const request = { body, query, params };
+      console.log('!------------->request:', JSON.stringify(request, '  ', null), '\nid:', id);
+      const user = await app.objection.models.user.query().where('id', id);
+      console.log('!------------->user:', JSON.stringify(user, '  ', null));
+      reply.render('users/edit', { user });
+      return reply;
     })
     .post('/users', async (req, reply) => {
       const user = new app.objection.models.user();
       user.$set(req.body.data);
-      console.log('!--->user: ', JSON.stringify(user, null, '  '));
-
       try {
         const validUser = await app.objection.models.user.fromJson(req.body.data);
-        console.log('!--->validUser: ', JSON.stringify(validUser, null, '  '));
         await app.objection.models.user.query().insert(validUser);
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
-      } // catch ({ data }) {
-      catch (e) {
-        console.error('!--->error: ', JSON.stringify(e, null, '  '));
-        const { data } = e;
-        console.error('!------>data: ', JSON.stringify(data, null, '  '));
+      } catch ({ data }) {
         req.flash('error', i18next.t('flash.users.create.error'));
         reply.render('users/new', { user, errors: data });
       }
-
+      return reply;
+    })
+    .patch('/users/:id', async (req, reply) => {
+      const { id } = req.params;
+      // const { query, params } = req;
+      // const request = { query, params };
+      // console.log(JSON.stringify(request, '  ', null));
+      const user = await app.objection.models.user.query().where('id', id);
+      try {
+        const validUser = await app.objection.models.user.fromJson(req.body.data);
+        await app.objection.models.user.query().findById(2).patch(validUser);
+        req.flash('info', i18next.t('flash.users.edit.success'));
+        reply.redirect(app.reverse('/users'));
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.users.edit.error'));
+        reply.render('users/edit', { user, errors: data });
+      }
       return reply;
     });
 };
