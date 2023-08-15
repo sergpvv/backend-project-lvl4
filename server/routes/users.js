61706831fc788343e422ca180e6e3928a5e743cc
sergpvv <serg.pvv@gmail.com>
2023-08-15 22:14:15 +0000

    add auth prevalidation

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,14 +14,9 @@ export default (app) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
     })
-    .get('/users/:id/edit', { name: 'editUser' }, async (req, reply) => {
+    .get('/users/:id/edit', { name: 'editUser', preValidation: app.authenticate }, async (req, reply) => {
       const { id } = req.params;
       const currentUserId = req.session.get('userId');
-      if (!currentUserId) {
-        req.flash('error', i18next.t('flash.authError'));
-        reply.redirect(app.reverse('root'));
-        return reply;
-      }
       if (!isEqual(id, currentUserId)) {
         req.flash('error', i18next.t('flash.accessDenied'));
         const users = await app.objection.models.user.query();
@@ -37,23 +32,22 @@ export default (app) => {
       user.$set(req.body.data);
       try {
         const validUser = await app.objection.models.user.fromJson(req.body.data);
-        // console.log('!--->validUser:', JSON.stringify(validUser, null, '  '));
         await app.objection.models.user.query().insert(validUser).debug();
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
       } catch ({ data }) {
-        // console.log('!--->catch error:', JSON.stringify(data, null, '  '));
         req.flash('error', i18next.t('flash.users.create.error'));
         reply.render('users/new', { user, errors: data });
       }
       return reply;
     })
-    .patch('/users/:id', { name: 'patchUser' }, async (req, reply) => {
+    .patch('/users/:id', { name: 'patchUser', preValidation: app.authenticate }, async (req, reply) => {
       const { id } = req.params;
       const currentUserId = req.session.get('userId');
-      if (!currentUserId || !isEqual(id, currentUserId)) {
-        req.flash('error', i18next.t('flash.suckerPunch'));
-        reply.code(418).redirect(app.reverse('root'));
+      if (!isEqual(id, currentUserId)) {
+        req.flash('error', i18next.t('flash.accessDenied'));
+        const users = await app.objection.models.user.query();
+        reply.render('users/index', { users });
         return reply;
       }
       const user = new app.objection.models.user();
@@ -70,14 +64,9 @@ export default (app) => {
       }
       return reply;
     })
-    .delete('/users/:id', { name: 'deleteUser' }, async (req, reply) => {
+    .delete('/users/:id', { name: 'deleteUser', preValidation: app.authenticate }, async (req, reply) => {
       const { id } = req.params;
       const currentUserId = req.session.get('userId');
-      if (!currentUserId) {
-        req.flash('error', i18next.t('flash.authError'));
-        reply.redirect(app.reverse('root'));
-        return reply;
-      }
       if (!isEqual(id, currentUserId)) {
         req.flash('error', i18next.t('flash.accessDenied'));
         const users = await app.objection.models.user.query();
