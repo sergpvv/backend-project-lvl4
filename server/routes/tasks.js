3bccdeff6d19213837398b1a40885bd97f60faf1
sergpvv <serg.pvv@gmail.com>
2023-08-19 20:05:43 +0000

    wip: task CRUD

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -3,32 +3,45 @@ import isEqual from '../helpers/isEqual.js';
 
 export default (app) => {
   app
-    .get('/tasks', { name: 'tasks' }, async (req, reply) => {
+    .get('/tasks', { name: 'tasks', preValidation: app.authenticate }, async (req, reply) => {
       const tasks = await app.objection.models.task.query();
       reply.render('tasks/index', { tasks });
       return reply;
     })
-    .get('/tasks/new', { name: 'newTask' }, (req, reply) => {
+    .get('/tasks/new', { name: 'newTask', preValidation: app.authenticate }, async (req, reply) => {
       const task = new app.objection.models.task();
+      task.labels = await app.objection.models.label.query();
+      task.statuses = await app.objection.models.taskStatus.query();
+      task.users = await app.objection.models.user.query();
+      // console.log('!------------->task:', JSON.stringify(task, null, '  '));
       reply.render('tasks/new', { task });
+      return reply;
     })
     .get('/tasks/:id/edit', { name: 'editTask', preValidation: app.authenticate }, async (req, reply) => {
       const task = await app.objection.models.task.query().findById(req.params.id);
       reply.render('tasks/edit', { task });
       return reply;
     })
-    .post('/tasks', { name: 'createNewTask' }, async (req, reply) => {
+    .post('/tasks', { name: 'createNewTask', preValidation: app.authenticate }, async (req, reply) => {
+      const task = new app.objection.models.task();
+      task.$set(req.body.data);
+      const statusId = parseInt(task.statusId, 10);
+      const executorId = parseInt(task.executorId, 10);
+      const creatorId = parseInt(req.session.get('userId'), 10);
+      task.$set({ statusId, creatorId, executorId });
       try {
-        const validTask = await app.objection.models.task.fromJson({
-          creatorId: req.session.get('userId'),
-          ...req.body.data,
-        });
+        console.log('!------------->task:', JSON.stringify(task, null, '  '));
+        const validTask = await app.objection.models.task.fromJson(task);
+        console.log('!------------->validTask:', JSON.stringify(validTask, null, '  '));
         await app.objection.models.task.query().insert(validTask);
         req.flash('info', i18next.t('flash.tasks.create.success'));
         reply.render(app.reverse('tasks'));
       } catch ({ data }) {
+        task.labels = await app.objection.models.label.query();
+        task.statuses = await app.objection.models.taskStatus.query();
+        task.users = await app.objection.models.user.query();
         req.flash('error', i18next.t('flash.tasks.create.error'));
-        reply.render('tasks/new', { task: req.body.data, errors: data });
+        reply.render('tasks/new', { task, errors: data });
       }
       return reply;
     })
